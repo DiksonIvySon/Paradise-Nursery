48c4486d75ec62258ef7f18ad6acdd46d0742f75
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2025-07-17 17:23:42 +0200

    Updated main.jsx to be wraped in Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
-// we'll add ProductPage later
+import ProductListing from './pages/ProductListing'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
-        {/* Placeholder route for products */}
+        <Route path="/products" element={<ProductListing />} />
         <Route path="/products" element={<div>Product Listing Page (coming soon)</div>} />
       </Routes>
     </Router>
